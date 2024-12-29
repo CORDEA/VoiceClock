38da2d8744ce13f10a13a22a609e1e3855ed9c59
Yoshihiro Tanaka <contact@cordea.jp>
2024-12-29 13:58:17 +0900

    update the layout

--- app/src/main/java/jp/cordea/voiceclock/ui/timer/Timer.kt
+++ app/src/main/java/jp/cordea/voiceclock/ui/timer/Timer.kt
@@ -1,7 +1,77 @@
 package jp.cordea.voiceclock.ui.timer
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Timer() {
+    Scaffold(
+        floatingActionButton = {
+            LargeFloatingActionButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.PlayArrow,
+                    contentDescription = "Play",
+                    modifier = Modifier.size(FloatingActionButtonDefaults.LargeIconSize)
+                )
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(32.dp))
+            Progress()
+        }
+    }
+}
+
+@Composable
+private fun Progress() {
+    val color = MaterialTheme.colorScheme.primary
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(32.dp)
+    ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .aspectRatio(1f)
+        ) {
+            drawArc(
+                color = color,
+                startAngle = 0f,
+                sweepAngle = 270f,
+                useCenter = false,
+                style = Stroke(width = 8.dp.toPx())
+            )
+        }
+        Text(
+            text = "00:00:00",
+            style = MaterialTheme.typography.headlineLarge,
+            fontSize = MaterialTheme.typography.headlineLarge.fontSize
+        )
+    }
 }
