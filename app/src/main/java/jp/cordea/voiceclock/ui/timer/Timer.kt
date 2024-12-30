f8b333c0c9fd046bfcb4795817f3bc5d78980c39
Yoshihiro Tanaka <contact@cordea.jp>
2024-12-30 12:13:35 +0900

    handle the controller

--- app/src/main/java/jp/cordea/voiceclock/ui/timer/Timer.kt
+++ app/src/main/java/jp/cordea/voiceclock/ui/timer/Timer.kt
@@ -3,39 +3,94 @@ package jp.cordea.voiceclock.ui.timer
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
+import jp.cordea.voiceclock.TtsState
+import jp.cordea.voiceclock.ui.clock.ClockUnit
 
 @Composable
 fun Timer(viewModel: TimerViewModel) {
+    val state by viewModel.uiState.collectAsState()
     Scaffold(
         floatingActionButton = {
-            LargeFloatingActionButton(onClick = {}) {
-                Icon(
-                    imageVector = Icons.Default.PlayArrow,
-                    contentDescription = "Play",
-                    modifier = Modifier.size(FloatingActionButtonDefaults.LargeIconSize)
-                )
+            when (state.ttsState) {
+                TtsState.INITIALIZED ->
+                    LargeFloatingActionButton(
+                        onClick = {
+                            viewModel.onFabClicked()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.PlayArrow,
+                            contentDescription = "Play",
+                            modifier = Modifier.size(FloatingActionButtonDefaults.LargeIconSize)
+                        )
+                    }
+
+                TtsState.ERROR -> {}
+                TtsState.LOADING -> LargeFloatingActionButton(onClick = {}) {
+                    CircularProgressIndicator()
+                }
             }
         }
     ) { innerPadding ->
+        if (state.showController) {
+            Controller(
+                isValueExpanded = state.isValueExpanded,
+                isUnitExpanded = state.isUnitExpanded,
+                value = state.value,
+                unit = state.unit,
+                onDismiss = {
+                    viewModel.onDismissController()
+                },
+                onValueExpandChanged = {
+                    viewModel.onValueExpandChanged(it)
+                },
+                onValueChanged = {
+                    viewModel.onValueChanged(it)
+                },
+                onUnitExpandChanged = {
+                    viewModel.onUnitExpandChanged(it)
+                },
+                onUnitChanged = {
+                    viewModel.onUnitChanged(it)
+                },
+                onClicked = {
+                    viewModel.onPlayClicked()
+                }
+            )
+        }
         Column(
             modifier = Modifier.padding(innerPadding),
             horizontalAlignment = Alignment.CenterHorizontally
@@ -73,5 +128,123 @@ private fun Progress() {
             style = MaterialTheme.typography.headlineLarge,
             fontSize = MaterialTheme.typography.headlineLarge.fontSize
         )
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun Controller(
+    isValueExpanded: Boolean,
+    isUnitExpanded: Boolean,
+    value: Int,
+    unit: ClockUnit,
+    onDismiss: () -> Unit,
+    onValueExpandChanged: (Boolean) -> Unit,
+    onValueChanged: (Int) -> Unit,
+    onUnitExpandChanged: (Boolean) -> Unit,
+    onUnitChanged: (ClockUnit) -> Unit,
+    onClicked: () -> Unit
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismiss
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Every")
+                Spacer(modifier = Modifier.width(16.dp))
+                ExposedDropdownMenuBox(
+                    modifier = Modifier.weight(1f),
+                    expanded = isValueExpanded,
+                    onExpandedChange = onValueExpandChanged
+                ) {
+                    TextField(
+                        modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryEditable),
+                        value = value.toString(),
+                        onValueChange = {},
+                        singleLine = true,
+                        trailingIcon = {
+                            ExposedDropdownMenuDefaults.TrailingIcon(
+                                expanded = isValueExpanded,
+                                modifier = Modifier.menuAnchor(MenuAnchorType.SecondaryEditable)
+                            )
+                        }
+                    )
+                    DropdownMenu(expanded = isValueExpanded, onDismissRequest = {
+                        onValueExpandChanged(false)
+                    }) {
+                        val range = when (unit) {
+                            ClockUnit.HOUR -> (0..23)
+                            ClockUnit.MINUTE -> (0..59)
+                            ClockUnit.SECOND -> (0..59)
+                        }
+                        range.forEach {
+                            DropdownMenuItem(
+                                onClick = {
+                                    onValueChanged(it)
+                                },
+                                text = {
+                                    Text(it.toString())
+                                },
+                                contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                            )
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                ExposedDropdownMenuBox(
+                    modifier = Modifier.weight(1f),
+                    expanded = isUnitExpanded,
+                    onExpandedChange = onUnitExpandChanged
+                ) {
+                    TextField(
+                        modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryEditable),
+                        value = when (unit) {
+                            ClockUnit.HOUR -> "hour"
+                            ClockUnit.MINUTE -> "minute"
+                            ClockUnit.SECOND -> "second"
+                        },
+                        onValueChange = {},
+                        singleLine = true,
+                        trailingIcon = {
+                            ExposedDropdownMenuDefaults.TrailingIcon(
+                                expanded = isUnitExpanded,
+                                modifier = Modifier.menuAnchor(MenuAnchorType.SecondaryEditable)
+                            )
+                        }
+                    )
+                    DropdownMenu(expanded = isUnitExpanded, onDismissRequest = {
+                        onUnitExpandChanged(false)
+                    }) {
+                        ClockUnit.entries.forEach {
+                            DropdownMenuItem(
+                                onClick = {
+                                    onUnitChanged(it)
+                                },
+                                text = {
+                                    when (it) {
+                                        ClockUnit.HOUR -> Text("hour")
+                                        ClockUnit.MINUTE -> Text("minute")
+                                        ClockUnit.SECOND -> Text("second")
+                                    }
+                                },
+                                contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                            )
+                        }
+
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(
+                onClick = onClicked,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Play")
+            }
+        }
     }
 }
--- app/src/main/java/jp/cordea/voiceclock/ui/timer/TimerViewModel.kt
+++ app/src/main/java/jp/cordea/voiceclock/ui/timer/TimerViewModel.kt
@@ -1,8 +1,93 @@
 package jp.cordea.voiceclock.ui.timer
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import jp.cordea.voiceclock.GetTtsStateUseCase
+import jp.cordea.voiceclock.TtsState
+import jp.cordea.voiceclock.ui.clock.ClockUnit
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
-class TimerViewModel @Inject constructor() {
+class TimerViewModel @Inject constructor(
+    getTtsStateUseCase: GetTtsStateUseCase
+) : ViewModel() {
+    private val remaining = MutableStateFlow("")
+    private val showController = MutableStateFlow(false)
+    private val isValueExpanded = MutableStateFlow(false)
+    private val isUnitExpanded = MutableStateFlow(false)
+    private val value = MutableStateFlow(1)
+    private val unit = MutableStateFlow(ClockUnit.MINUTE)
+    private val isStarted = MutableStateFlow(false)
+
+    val uiState =
+        combine(
+            remaining,
+            showController,
+            combine(
+                isValueExpanded,
+                isUnitExpanded
+            ) { isValueExpanded, isUnitExpanded ->
+                isValueExpanded to isUnitExpanded
+            },
+            isStarted,
+            getTtsStateUseCase.execute()
+        ) { remaining, showController, expanded, isStarted, state ->
+            TimerUiState(
+                remaining,
+                state,
+                showController,
+                expanded.first,
+                expanded.second,
+                unit.value,
+                value.value,
+                isStarted
+            )
+        }.stateIn(
+            viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            TimerUiState(
+                "",
+                TtsState.LOADING,
+                showController = false,
+                isValueExpanded = false,
+                isUnitExpanded = false,
+                ClockUnit.MINUTE,
+                1,
+                isStarted = false
+            )
+        )
+
+    fun onFabClicked() {
+        showController.value = true
+    }
+
+    fun onPlayClicked() {
+        showController.value = false
+        isStarted.value = true
+    }
+
+    fun onValueExpandChanged(it: Boolean) {
+        isValueExpanded.value = it
+    }
+
+    fun onValueChanged(it: Int) {
+        value.value = it
+    }
+
+    fun onUnitExpandChanged(it: Boolean) {
+        isUnitExpanded.value = it
+    }
+
+    fun onUnitChanged(it: ClockUnit) {
+        unit.value = it
+    }
+
+    fun onDismissController() {
+        showController.value = false
+    }
 }
