48d2dc62754e77cb6e21ec72a430927dddba30c5
Yoshihiro Tanaka <contact@cordea.jp>
2025-06-28 15:08:54 +0900

    fix the reading timing

--- app/src/main/java/jp/cordea/voiceclock/ShouldReadTimerTextUseCase.kt
+++ app/src/main/java/jp/cordea/voiceclock/ShouldReadTimerTextUseCase.kt
@@ -7,28 +7,9 @@ import javax.inject.Inject
 @Reusable
 class ShouldReadTimerTextUseCase @Inject constructor() {
     fun execute(current: Duration, timing: Long): Boolean {
-        val seconds = current.seconds
-        if (current == Duration.ZERO || seconds < timing) {
+        if (current == Duration.ZERO || timing <= 0L) {
             return false
         }
-        if (timing == 0L) {
-            return seconds % 60 == 0L
-        }
-        if (seconds == timing) {
-            return true
-        }
-        val timingInMinutes = timing / 60L
-        if (timingInMinutes < 1) {
-            return (seconds - timing) % 60 == 0L
-        }
-        val timingInHours = timing / 3600L
-        if (timingInHours < 1) {
-            return (current.toMinutes() - timingInMinutes) % 60 == 0L &&
-                    seconds % 60 == 0L
-        }
-        val hours = current.toHours()
-        return (hours - timingInHours) % 24 == 0L &&
-                current.toMinutes() % 60 == 0L &&
-                seconds % 60 == 0L
+        return current.seconds % timing == 0L
     }
 }
--- app/src/test/java/jp/cordea/voiceclock/ShouldReadTimerTextUseCaseTest.kt
+++ app/src/test/java/jp/cordea/voiceclock/ShouldReadTimerTextUseCaseTest.kt
@@ -10,15 +10,12 @@ class ShouldReadTimerTextUseCaseTest {
         val useCase = ShouldReadTimerTextUseCase()
 
         assertThat(useCase.execute(Duration.ZERO, 10)).isFalse()
-
-        assertThat(useCase.execute(Duration.ofMinutes(1), 0)).isTrue()
-        assertThat(useCase.execute(Duration.ofMinutes(2), 0)).isTrue()
-        assertThat(useCase.execute(Duration.ofHours(1), 0)).isTrue()
+        assertThat(useCase.execute(Duration.ofSeconds(1), 0)).isFalse()
 
         assertThat(useCase.execute(Duration.ofSeconds(40), 40)).isTrue()
-        assertThat(useCase.execute(Duration.ofSeconds(100), 40)).isTrue()
-        assertThat(useCase.execute(Duration.ofSeconds(80), 40)).isFalse()
-        assertThat(useCase.execute(Duration.ofSeconds(360), 40)).isFalse()
+        assertThat(useCase.execute(Duration.ofSeconds(100), 40)).isFalse()
+        assertThat(useCase.execute(Duration.ofSeconds(80), 40)).isTrue()
+        assertThat(useCase.execute(Duration.ofSeconds(360), 40)).isTrue()
         assertThat(useCase.execute(Duration.ofSeconds(1), 40)).isFalse()
         assertThat(useCase.execute(Duration.ofSeconds(50), 40)).isFalse()
         assertThat(useCase.execute(Duration.ofSeconds(60), 40)).isFalse()
@@ -29,22 +26,20 @@ class ShouldReadTimerTextUseCaseTest {
         assertThat(useCase.execute(Duration.ofSeconds(61), 60)).isFalse()
         assertThat(useCase.execute(Duration.ofSeconds(110), 60)).isFalse()
         assertThat(useCase.execute(Duration.ofSeconds(59), 60)).isFalse()
-        assertThat(useCase.execute(Duration.ofMinutes(2), 60)).isFalse()
-        assertThat(useCase.execute(Duration.ofMinutes(50), 60)).isFalse()
-        assertThat(useCase.execute(Duration.ofMinutes(11), 60)).isFalse()
+        assertThat(useCase.execute(Duration.ofMinutes(2), 60)).isTrue()
+        assertThat(useCase.execute(Duration.ofMinutes(50), 60)).isTrue()
 
         assertThat(useCase.execute(Duration.ofMinutes(2), 120)).isTrue()
         assertThat(useCase.execute(Duration.ofMinutes(62), 120)).isTrue()
         assertThat(useCase.execute(Duration.ofMinutes(121), 120)).isFalse()
-        assertThat(useCase.execute(Duration.ofMinutes(50), 120)).isFalse()
+        assertThat(useCase.execute(Duration.ofMinutes(50), 120)).isTrue()
         assertThat(useCase.execute(Duration.ofMinutes(11), 120)).isFalse()
 
         assertThat(useCase.execute(Duration.ofHours(1), 3600)).isTrue()
         assertThat(useCase.execute(Duration.ofSeconds(3601), 3600)).isFalse()
         assertThat(useCase.execute(Duration.ofSeconds(3660), 3600)).isFalse()
-        assertThat(useCase.execute(Duration.ofHours(2), 3600)).isFalse()
-        assertThat(useCase.execute(Duration.ofHours(3), 3600)).isFalse()
-        assertThat(useCase.execute(Duration.ofHours(50), 3600)).isFalse()
-        assertThat(useCase.execute(Duration.ofHours(11), 3600)).isFalse()
+        assertThat(useCase.execute(Duration.ofHours(2), 3600)).isTrue()
+        assertThat(useCase.execute(Duration.ofHours(3), 3600)).isTrue()
+        assertThat(useCase.execute(Duration.ofHours(50), 3600)).isTrue()
     }
 }
